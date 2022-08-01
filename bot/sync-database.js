0ae9dc6ed86af22d92178309a745a1ad832df5dc
Jack Balster <107078414+jack-balster@users.noreply.github.com>
2022-08-01 13:30:50 -0700

    Update sync-database.js

--- bot/sync-database.js
+++ bot/sync-database.js
@@ -1,14 +1,19 @@
+//creates all our database tables whenever it is run
+//import sequelize package
 const Sequelize = require('sequelize');
 
+//instance of sequelize name, username, password for database
 const sequelize = new Sequelize('discordbot', 'username', 'password', {
-    host: 'localhost',
+    host: 'localhost', //database stored on computer
     dialect: 'sqlite',
     logging: false,
-    storage: 'database.sqlite',
+    storage: 'database.sqlite', //name of file to store database contents (file is binary)
 });
 
+//import tictactoe model
 require('./models/tictactoe.js')(sequelize, Sequelize.DataTypes);
 
+//make sure database matches model specified (helpful if future changes are made to tictactoe model)
 sequelize.sync().then(async () => {
     console.log('Database synced');
     sequelize.close();
