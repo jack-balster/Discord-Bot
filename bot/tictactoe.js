8137d5f853e218dc3b702522d8e0cb31c855b726
Jack Balster <107078414+jack-balster@users.noreply.github.com>
2022-08-01 13:16:04 -0700

    Update tictactoe.js

--- bot/tictactoe.js
+++ bot/tictactoe.js
@@ -1,12 +1,14 @@
+//export sequelize model for tictactoe table
 module.exports = (sequelize, DataTypes) => {
-    return sequelize.define('tictactoe', {
+    return sequelize.define('tictactoe', { //table named tictactoe
+        //convert everything to sequelize syntax (already know what DB table should look like from MIS 325)
         user_id: {
             type: DataTypes.STRING,
-            primaryKey: true,
+            primaryKey: true, //unique users
         },
         score: {
             type: DataTypes.INTEGER,
-            defaultValue: 0,
+            defaultValue: 0, //starts at 0 and will increment by 1 if they win
             allowNull: false,
         }
     })
